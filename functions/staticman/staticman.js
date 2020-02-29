316563df5442a51ae38c49eac14ebacc985d9398
Adrian Yadav <adrianyadav@gmail.com>
2020-02-29 18:22:13 +1100

    restored staticman to original

--- functions/staticman/staticman.js
+++ functions/staticman/staticman.js
@@ -1,33 +1,36 @@
 const qs = require('qs');
 const UrlPattern = require('url-pattern');
+const Staticman = require('staticman/lib/Staticman');
+
 const pattern = new UrlPattern(
 	'/.netlify/functions/staticman/:username/:repository/:branch/:property'
 );
 
-let urlParams = '';
-let staticman = '';
-let params = {
-	...urlParams,
-	// staticman lib has different logic per version. Lock it to 2
-	version: '2'
-};
-let body = '';
-let options = '';
-
 exports.handler = (event, context, callback) => {
-	params = {
+	const urlParams = pattern.match(event.path);
+	const params = {
 		...urlParams,
 		// staticman lib has different logic per version. Lock it to 2
 		version: '2'
 	};
 
-	urlParams = pattern.match(event.path);
-	staticman = new Staticman(params);
+	const staticman = new Staticman(params);
 	staticman.setConfigPath();
 	staticman.setIp(event.headers['client-ip']);
 	staticman.setUserAgent(event.headers['user-agent']);
-	body = qs.parse(event.body);
-	let options = body.options || {};
-	const StaticmanAPI = require('staticman/lib/Staticman');
-	const api = new StaticmanAPI(params);
+
+	const body = qs.parse(event.body);
+	const options = body.options || {};
+
+	staticman
+		.processEntry(body.fields, options)
+		.then(() => {
+			callback(null, {
+				statusCode: 200,
+				body: 'Success'
+			});
+		})
+		.catch(error => {
+			callback(error);
+		});
 };

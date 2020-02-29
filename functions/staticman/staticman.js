5d2cc3868e0ff856068ba40d978481ef69c79153
Adrian Yadav <adrianyadav@gmail.com>
2020-02-29 17:41:29 +1100

    testing staticman api

--- functions/staticman/staticman.js
+++ functions/staticman/staticman.js
@@ -1,25 +1,41 @@
 const qs = require('qs');
 const UrlPattern = require('url-pattern');
-const Staticman = require('staticman/lib/Staticman');
-
 const pattern = new UrlPattern(
 	'/.netlify/functions/staticman/:username/:repository/:branch/:property'
 );
 
+let urlParams = '';
+let staticman = '';
+let params = {
+	...urlParams,
+	// staticman lib has different logic per version. Lock it to 2
+	version: '2'
+};
+let body = '';
+let options = '';
+
 exports.handler = (event, context, callback) => {
-	const urlParams = pattern.match(event.path);
-	const params = {
+	params = {
 		...urlParams,
 		// staticman lib has different logic per version. Lock it to 2
 		version: '2'
 	};
 
-	const staticman = new Staticman(params);
+	urlParams = pattern.match(event.path);
+	staticman = new Staticman(params);
+	staticman.setConfigPath();
+	staticman.setIp(event.headers['client-ip']);
+	staticman.setUserAgent(event.headers['user-agent']);
+	body = qs.parse(event.body);
+	let options = body.options || {};
+};
+try {
+	const StaticmanAPI = require('staticman/lib/Staticman');
+	const api = new StaticmanAPI(params);
 
-	const body = qs.parse(event.body);
-	const options = body.options || {};
-
-	staticman.start(port => {
+	api.start(port => {
 		console.log('Staticman API running on port', port);
 	});
-};
+} catch (e) {
+	console.error(e);
+}

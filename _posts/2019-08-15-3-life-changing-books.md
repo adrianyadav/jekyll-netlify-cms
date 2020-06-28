e88e3a6fa1818248eb10c482e9f2a8e0c47dfa50
Adrian Yadav <adrianyadav@gmail.com>
2020-06-29 00:36:52 +1000

    cleanup

--- _posts/2019-08-15-3-life-changing-books.md
+++ _posts/2019-08-15-3-life-changing-books.md
@@ -3,28 +3,30 @@ layout: post
 title: 3 Life changing books
 featuredImage: /img/uploads/08.jpg
 tags: Work
+comments: true
 ---
+
 I hesitate to use "**Life-changing**" as it is way too casually thrown out but it is definitely applicable here. While I was traveling, I was looking for a few audio books to digest on the flight(s) and came across these three books.
 
 ## 1. Why We Sleep - By Matthew Walker
 
-If you are not getting 8-9 hours of quality (uninterrupted) sleep on a daily basis - then you need to make a change. 
+If you are not getting 8-9 hours of quality (uninterrupted) sleep on a daily basis - then you need to make a change.
 
-Sleep is the **greatest** performance enhancer we have available to us - why not make the most use of it? 
+Sleep is the **greatest** performance enhancer we have available to us - why not make the most use of it?
 
 Top athletes like LeBron James, Usain Bolt, Roger Federer - routinely get 8-12 hours of sleep on a daily basis. The book explains in detail how sleep benefits you and what the downsides are of not getting enough sleep.
 
-Matthew Walker makes a good case for how we don't value sleep on a societal level and this should be considered an epidemic. For example, we respect the CEO that comes to work at 6AM and responds to an email at 1AM and applaud him for how hardworking and dedicated he is but is he efficient as he could be? 
+Matthew Walker makes a good case for how we don't value sleep on a societal level and this should be considered an epidemic. For example, we respect the CEO that comes to work at 6AM and responds to an email at 1AM and applaud him for how hardworking and dedicated he is but is he efficient as he could be?
 
 Not necessarily - **bottom line** is you need more quality sleep which will make you do better in every aspect of your life.
 
 ## 2. Atomic Habits - By James Clear
 
-> We are what we do.  Excellence, then, is not an act, but a habit
+> We are what we do. Excellence, then, is not an act, but a habit
 >
->  —  Will Durant
+> —  Will Durant
 
-The book is a comprehensive guide on how habits are formed, tips on how to make the good ones stay  and eliminate the bad ones.
+The book is a comprehensive guide on how habits are formed, tips on how to make the good ones stay and eliminate the bad ones.
 
 When you have a new habit that you want to form, for example: I want to get fit. Then this book will give you the tools to make sure that you can make that habit stick and incorporate it in your daily routine.
 
@@ -35,11 +37,11 @@ James introduces these 4 laws that can be used to form a habit.
 3. Make it **easy**
 4. Make it **satisfying**
 
-How do you incorporate the above? Well it depends on the habit. 
+How do you incorporate the above? Well it depends on the habit.
 
-Let's say that I want to get fit. 
+Let's say that I want to get fit.
 
-Then, to make it "easy" - I could say that every day I will go to the gym but I will only exercise for 5 minutes. I can leave after that point - and I've accomplished my goal. 
+Then, to make it "easy" - I could say that every day I will go to the gym but I will only exercise for 5 minutes. I can leave after that point - and I've accomplished my goal.
 
 If I stay consistent and do this every day, at some point I will feel that, hey five minutes isn't that long of a time - let's do 10 minutes and so on. The idea is to start small and build up slowly, if your goal is too ambitious (e.g. I will do 100 push ups every day) from day one then it's going to be difficult to form this habit.
 
@@ -53,6 +55,6 @@ Lots more practical examples are included in the book, I highly recommend you at
 
 David Goggins has done some incredible things: become a navy seal, held a record for the most pull ups in a day, completing Badwater-135 Ultra marathon (which is described as world's toughest footrace) - and the list goes on.
 
-It requires an enormous amount of mental strength, fortitude and resilience  to do these things. In this book, David shares what helped him develop the mental strength to push through and overcome any obstacle that came his way or that he put there. 
+It requires an enormous amount of mental strength, fortitude and resilience to do these things. In this book, David shares what helped him develop the mental strength to push through and overcome any obstacle that came his way or that he put there.
 
 The main takeaway from this book, is that if you are too comfortable - you need to make a **change**. Being comfortable means you cannot quickly grow as a person. When life gets too easy, add something to make it **difficult**. For example: waking up every day at 6am - when that get's easy, make it harder - go for a run after you wake up.
--- js/common.js
+++ js/common.js
@@ -1,115 +1,115 @@
-$(document).ready(function() {
-	'use strict';
+$(document).ready(function () {
+  "use strict";
 
-	var menuOpenIcon = $('.icon--menu'),
-		menuCloseIcon = $('.icon--close-menu'),
-		menuList = $('.navigation'),
-		searchOpenIcon = $('.icon--search'),
-		searchCloseIcon = $('.icon--close-search'),
-		searchBox = $('.search'),
-		header = $('.header'),
-		main = $('#main');
+  var menuOpenIcon = $(".icon--menu"),
+    menuCloseIcon = $(".icon--close-menu"),
+    menuList = $(".navigation"),
+    searchOpenIcon = $(".icon--search"),
+    searchCloseIcon = $(".icon--close-search"),
+    searchBox = $(".search"),
+    header = $(".header"),
+    main = $("#main");
 
-	/* =======================
+  /* =======================
   // Menu and Search
   ======================= */
-	menuOpenIcon.click(function() {
-		menuOpen();
-		addOverlay();
-	});
+  menuOpenIcon.click(function () {
+    menuOpen();
+    addOverlay();
+  });
 
-	menuCloseIcon.click(function() {
-		menuClose();
-		removeOverlay();
-	});
+  menuCloseIcon.click(function () {
+    menuClose();
+    removeOverlay();
+  });
 
-	searchOpenIcon.click(function() {
-		searchOpen();
-	});
+  searchOpenIcon.click(function () {
+    searchOpen();
+  });
 
-	searchCloseIcon.click(function() {
-		searchClose();
-	});
+  searchCloseIcon.click(function () {
+    searchClose();
+  });
 
-	function menuOpen() {
-		menuList.addClass('navigation--active');
-		addOverlay();
-	}
+  function menuOpen() {
+    menuList.addClass("navigation--active");
+    addOverlay();
+  }
 
-	function menuClose() {
-		menuList.removeClass('navigation--active');
-		removeOverlay();
-	}
+  function menuClose() {
+    menuList.removeClass("navigation--active");
+    removeOverlay();
+  }
 
-	function searchOpen() {
-		searchBox.addClass('is-visible');
-	}
+  function searchOpen() {
+    searchBox.addClass("is-visible");
+  }
 
-	function searchClose() {
-		searchBox.removeClass('is-visible');
-	}
+  function searchClose() {
+    searchBox.removeClass("is-visible");
+  }
 
-	function addOverlay() {
-		header.addClass('overlay');
-		main.addClass('overlay');
-	}
+  function addOverlay() {
+    header.addClass("overlay");
+    main.addClass("overlay");
+  }
 
-	function removeOverlay() {
-		header.removeClass('overlay');
-		main.removeClass('overlay');
-	}
+  function removeOverlay() {
+    header.removeClass("overlay");
+    main.removeClass("overlay");
+  }
 
-	/* =======================
+  /* =======================
   // Zoom Image
   ======================= */
-	$('.page img, .post img').attr('data-action', 'zoom');
-	$('.page a img, .post a img').removeAttr('data-action', 'zoom');
+  $(".page img, .post img").attr("data-action", "zoom");
+  $(".page a img, .post a img").removeAttr("data-action", "zoom");
 });
 
 // Static comments
-(function($) {
-	var $comments = $('.js-comments');
+(function ($) {
+  var $comments = $(".js-comments");
 
-	$('#comment-form').submit(function() {
-		var form = this;
+  $("#comment-form").submit(function () {
+    var form = this;
 
-		$(form).addClass('disabled');
-		$('#comment-form-submit').html(
-			'<svg class="icon spin"><use xlink:href="#icon-loading"></use></svg> Loading...'
-		);
+    $(form).addClass("disabled");
+    $("#comment-form-submit").html(
+      '<svg class="icon spin"><use xlink:href="#icon-loading"></use></svg> Loading...'
+    );
 
-		$.ajax({
-			type: $(this).attr('method'),
-			url: $(this).attr('action'),
-			data: $(this).serialize(),
-			contentType: 'application/x-www-form-urlencoded',
-			success: function(data) {
-				$('#comment-form-submit').html('Submitted');
-				$('.post__comments-form .js-notice')
-					.removeClass('notice--danger')
-					.addClass('notice--success');
-				showAlert(
-					'<strong>Thanks for your comment!</strong> It will show on the site once it has been approved.'
-				);
-			},
-			error: function(err) {
-				console.log(err);
-				$('#comment-form-submit').html('Submit Comment');
-				$('.post__comments-form .js-notice')
-					.removeClass('notice--success')
-					.addClass('notice--danger');
-				showAlert(
-					'<strong>Sorry, there was an error with your submission.</strong> Please make sure all required fields have been completed and try again.'
-				);
-				$(form).removeClass('disabled');
-			}
-		});
+    $.ajax({
+      type: $(this).attr("method"),
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+      contentType: "application/x-www-form-urlencoded",
+      success: function (data) {
+        $("#comment-form-submit").html("Submitted");
+        $(".post__comments-form .js-notice")
+          .removeClass("notice--danger")
+          .addClass("notice--success");
+        showAlert(
+          "<strong>Thanks for your comment!</strong> It will show on the site soon."
+        );
+      },
+      error: function (err) {
+        console.log(err);
+        $("#comment-form-submit").html("Submit Comment");
+        $(".post__comments-form .js-notice")
+          .removeClass("notice--success")
+          .addClass("notice--danger");
+        showAlert(
+          "<strong>Sorry, there was an error with your submission.</strong> Please make sure all required fields have been completed and try again."
+        );
+        $(form).removeClass("disabled");
+      },
+    });
 
-		return false;
-	});
+    return false;
+  });
 
-	function showAlert(message) {
-		$('.post__comments-form .js-notice').removeClass('hidden');
-		$('.post__comments-form .js-notice-text').html(message);
-	}
+  function showAlert(message) {
+    $(".post__comments-form .js-notice").removeClass("hidden");
+    $(".post__comments-form .js-notice-text").html(message);
+  }
 })(jQuery);
